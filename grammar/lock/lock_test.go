1fae23f694853157d3c9bf9def5a8db90791b06d
zhangzongliang <zhangzongliang@zuoshouyisheng.com>
2025-01-17 19:57:32 +0800

    Feat: 添加锁的使用举例

--- grammar/lock/lock_test.go
+++ grammar/lock/lock_test.go
@@ -8,9 +8,15 @@ import (
 
 const cost = time.Microsecond * 10
 
+// ** benchmark 函数用于测试读写锁的性能 **
+
+// 参数 b 是测试对象，rw 是读写锁，read 是读操作的次数，write 是写操作的次数
 func benchmark(b *testing.B, rw RW, read, write int) {
+	// 循环测试次数
 	for i := 0; i < b.N; i++ {
+		// 创建一个等待组，用于等待所有 goroutine 完成
 		var wg sync.WaitGroup
+		// 循环读操作次数
 		for k := 0; k < read*100; k++ {
 			wg.Add(1)
 			go func() {
@@ -18,6 +24,7 @@ func benchmark(b *testing.B, rw RW, read, write int) {
 				wg.Done()
 			}()
 		}
+		// 循环写操作次数
 		for k := 0; k < write*100; k++ {
 			wg.Add(1)
 			go func() {
@@ -25,6 +32,7 @@ func benchmark(b *testing.B, rw RW, read, write int) {
 				wg.Done()
 			}()
 		}
+		// 等待所有 goroutine 完成
 		wg.Wait()
 	}
 }
